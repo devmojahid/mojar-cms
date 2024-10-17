5865afeed325be9f9952e0018c9a1767bd17dadb
Mojahid <raofahmedmojahid@gmail.com>
2024-10-17 13:11:32 +0600

    menu setup

--- modules/Backend/resources/views/backend/menu_left.blade.php
+++ modules/Backend/resources/views/backend/menu_left.blade.php
@@ -1,4 +1,4 @@
-<ul class="navbar-nav pt-lg-3">
+<ul class="juzaweb__menuLeft__navigation">
     @php
         use Juzaweb\CMS\Facades\HookAction;
         use Juzaweb\CMS\Support\MenuCollection;
@@ -12,24 +12,24 @@
         $items = MenuCollection::make(apply_filters('get_admin_menu', HookAction::getAdminMenu()));
     @endphp
 
-    @foreach ($items as $item)
-        @if ($item->get('key') != 'dashboard' && !$jw_user->canAny($item->get('permissions', ['admin'])))
+    @foreach($items as $item)
+        @if($item->get('key') != 'dashboard' && !$jw_user->canAny($item->get('permissions', ['admin'])))
             @continue
         @endif
 
-        @if ($item->hasChildren())
+        @if($item->hasChildren())
             @php
                 $strChild = '';
                 $hasActive = false;
-                foreach ($item->getChildrens() as $child) {
-                    if (!$jw_user->canAny($child->get('permissions', ['admin']))) {
+                foreach($item->getChildrens() as $child) {
+                    if(!$jw_user->canAny($child->get('permissions', ['admin']))) {
                         continue;
                     }
 
                     if (empty($segment2)) {
                         $active = empty($child->getUrl());
                     } else {
-                        $active = request()->is($adminPrefix . '/' . $child->get('url') . '*');
+                        $active = request()->is($adminPrefix .'/'. $child->get('url') . '*');
                     }
 
                     if ($active) {
@@ -40,38 +40,26 @@
                         'adminUrl' => $adminUrl,
                         'item' => $child,
                         'active' => $active,
-                        'icon' => false,
+                        'icon' => false
                     ])->render();
                 }
             @endphp
 
-            <li
-                class="nav-item juzaweb__menuLeft__item juzaweb__menuLeft__submenu juzaweb__menuLeft__item-{{ $item->get('slug') }} @if ($hasActive) juzaweb__menuLeft__submenu--toggled @endif">
+            <li class="juzaweb__menuLeft__item juzaweb__menuLeft__submenu juzaweb__menuLeft__item-{{ $item->get('slug') }} @if($hasActive) juzaweb__menuLeft__submenu--toggled @endif">
                 <span class="juzaweb__menuLeft__item__link">
                     <i class="juzaweb__menuLeft__item__icon {{ $item->get('icon') }}"></i>
                     <span class="juzaweb__menuLeft__item__title">{{ $item->get('title') }}</span>
                 </span>
 
-                {{-- <ul class="juzaweb__menuLeft__navigation"
-                    @if ($hasActive) style="display: block;" @endif>
+                <ul class="juzaweb__menuLeft__navigation" @if($hasActive) style="display: block;" @endif>
                     {!! $strChild !!}
-                </ul> --}}
-                <div class="dropdown-menu {{ $item->get('active') ? 'show' : '' }}">
-                    <div class="dropdown-menu-columns">
-                        <div class="dropdown-menu-column">
-                            {!! $strChild !!}
-                        </div>
-                    </div>
-                </div>
+                </ul>
             </li>
         @else
             @component('cms::backend.items.menu_left_item', [
                 'adminUrl' => $adminUrl,
                 'item' => $item,
-                'active' =>
-                    $item->get('url') == 'dashboard'
-                        ? request()->is($adminPrefix)
-                        : request()->is($adminPrefix . '/' . $item->get('url') . '*'),
+                'active' => $item->get('url') == 'dashboard' ? request()->is($adminPrefix) : request()->is($adminPrefix .'/'. $item->get('url') . '*'),
             ])
             @endcomponent
         @endif
