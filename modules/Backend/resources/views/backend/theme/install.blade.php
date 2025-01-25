3835d59254446614b4eaaba2738f83a12ea434ea
Mojahid <raofahmedmojahid@gmail.com>
2025-01-25 16:16:17 +0600

    theme plugin install basic design change

--- modules/Backend/resources/views/backend/theme/install.blade.php
+++ modules/Backend/resources/views/backend/theme/install.blade.php
@@ -1,7 +1,56 @@
 @extends('cms::layouts.backend')
 
 @section('content')
-    <div class="row box-hidden mb-2" id="form-theme-upload">
+
+<div class="col-md-12 col-lg-12">
+    <div class="card">
+      <div class="card-header">
+        <ul class="nav nav-tabs card-header-tabs">
+          <li class="nav-item">
+            <a class="nav-link active" href="#">
+              Themes
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">
+              Upload Theme
+            </a>
+          </li>
+          <li class="nav-item ms-auto">
+            <button type="button" class="btn btn-tabler" id="upload-theme">
+              <!-- Download SVG icon from http://tabler-icons.io/i/settings -->
+              <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M10.325 4.317c.426 -1.756 2.924 -1.756 3.35 0a1.724 1.724 0 0 0 2.573 1.066c1.543 -.94 3.31 .826 2.37 2.37a1.724 1.724 0 0 0 1.065 2.572c1.756 .426 1.756 2.924 0 3.35a1.724 1.724 0 0 0 -1.066 2.573c.94 1.543 -.826 3.31 -2.37 2.37a1.724 1.724 0 0 0 -2.572 1.065c-.426 1.756 -2.924 1.756 -3.35 0a1.724 1.724 0 0 0 -2.573 -1.066c-1.543 .94 -3.31 -.826 -2.37 -2.37a1.724 1.724 0 0 0 -1.065 -2.572c-1.756 -.426 -1.756 -2.924 0 -3.35a1.724 1.724 0 0 0 1.066 -2.573c-.94 -1.543 .826 -3.31 2.37 -2.37c1 .608 2.296 .07 2.572 -1.065z" /><path d="M9 12a3 3 0 1 0 6 0a3 3 0 0 0 -6 0" /></svg>
+              {{ trans('cms::app.upload_theme') }}
+            </button>
+          </li>
+        </ul>
+      </div>
+      <div class="card-body">
+        <div class="theme-upload-form">
+            <div class="row box-hidden mb-2" id="form-theme-upload" style="display: none;">
+                <div class="col-md-12">
+                    <form action="{{ route('admin.theme.install.upload') }}" role="form" id="themeUploadForm"
+                        name="themeUploadForm" method="post" class="dropzone" enctype="multipart/form-data">
+                        <div class="form-group">
+                            <div class="controls text-center">
+                                <div class="input-group w-100">
+                                    <a class="btn btn-primary w-100 text-white"
+                                        id="theme-upload-button">{{ trans('cms::filemanager.message-choose') }}</a>
+                                </div>
+                            </div>
+                        </div>
+        
+                        <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                    </form>
+                </div>
+            </div>
+        </div>
+        <div class="row" id="theme-list"></div>
+      </div>
+    </div>
+  </div>
+
+    {{-- <div class="row box-hidden mb-2" id="form-theme-upload">
         <div class="col-md-12">
             <form action="{{ route('admin.theme.install.upload') }}" role="form" id="themeUploadForm" name="themeUploadForm"
                 method="post" class="dropzone" enctype="multipart/form-data">
@@ -17,19 +66,7 @@
                 <input type="hidden" name="_token" value="{{ csrf_token() }}">
             </form>
         </div>
-    </div>
-
-    <div class="row">
-        <div class="col-md-8"></div>
-
-        <div class="col-md-4 text-right">
-            <button type="button" class="btn btn-success" id="upload-theme">
-                {{ trans('cms::app.upload_theme') }}
-            </button>
-        </div>
-    </div>
-
-    <div class="row" id="theme-list"></div>
+    </div> --}}
 
     <script>
         function themeItemFormatter(index, row) {
@@ -65,12 +102,12 @@
             </div>`;
         }
 
-        const listView = new MojarListView({
-            url: "{{ route('admin.theme.install.all') }}",
-            list: "#theme-list",
-            page_size: 9,
-            item_formatter: "themeItemFormatter"
-        });
+        // const listView = new MojarListView({
+        //     url: "{{ route('admin.theme.install.all') }}",
+        //     list: "#theme-list",
+        //     page_size: 9,
+        //     item_formatter: "themeItemFormatter"
+        // });
 
         Dropzone.autoDiscover = false;
 
